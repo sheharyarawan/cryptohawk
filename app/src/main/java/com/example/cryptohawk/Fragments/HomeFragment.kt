90cd32c59d4144749faf40bc645e4ec63c79858c
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-04-22 12:52:51 +0500

    fetched data in the viewModel and also setup adapter , observe VM and setup Market data in Fragment Home

--- app/src/main/java/com/example/cryptohawk/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/cryptohawk/Fragments/HomeFragment.kt
@@ -5,7 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cryptohawk.Model.CryptoResponseItem
 import com.example.cryptohawk.R
+import com.example.cryptohawk.RecyclerView.cryptoListAdapter
+import com.example.cryptohawk.ViewModel.cryptoViewModel
+import com.example.cryptohawk.databinding.FragmentHomeBinding
+import kotlin.getValue
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    lateinit var binding: FragmentHomeBinding
+    val viewModel: cryptoViewModel by activityViewModels()
+    lateinit var adapter: cryptoListAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpAdapter()
+        observeVm()
+        setUpMarketData()
+        viewModel.getCryptoList("usd", "market_cap_desc", 5)
+    }
+
+    fun setUpAdapter() {
+        adapter = cryptoListAdapter { coin: CryptoResponseItem ->
+
+            viewModel.selectCoin(coin)
+            // findNavController().navigate(R.id.action_homeFragment_to_detailsFragment)
+
+        }
+        binding.homeRv.apply {
+            adapter = thiscom.example.demo.fragments.HomeFragment.adapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    fun observeVm() {
+        viewModel.cryptoList.observe(viewLifecycleOwner) { value ->
+            adapter.differ.submitList(value)
+        }
+        viewModel.error.observe(viewLifecycleOwner) { errorMessage ->
+            if (!errorMessage.isNullOrEmpty()) {
+                Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun setUpMarketData() {
+
+        binding.marketVolume.text =
+            "MARKET VOLUME\n ${viewModel.marketData.value?.data?.total_volume?.usd.toString()}"
+        binding.marketCap.text =
+            "MARKET CAP\n ${viewModel.marketData.value?.data?.total_market_cap?.usd}"
+        binding.activeCrypto.text =
+            "ACTIVE CRYPTO\n ${viewModel.marketData.value?.data?.active_cryptocurrencies}"
+        binding.marketChange.text =
+            "MARKET CHANGE\n ${viewModel.marketData.value?.data?.market_cap_percentage} "
+    }
 }
--- app/src/main/java/com/example/cryptohawk/ViewModel/cryptoViewModel.kt
+++ app/src/main/java/com/example/cryptohawk/ViewModel/cryptoViewModel.kt
@@ -2,6 +2,74 @@ package com.example.cryptohawk.ViewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.cryptohawk.Model.CryptoResponse
+import com.example.cryptohawk.Model.CryptoResponseItem
+import com.example.cryptohawk.Model.MarketResponse
 import com.example.cryptohawk.Repository.cryptoRepository
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class cryptoViewModel(app: Application, val repo: cryptoRepository): AndroidViewModel(app)
+class cryptoViewModel(app: Application, val repo: cryptoRepository): AndroidViewModel(app) {
+
+    private val _cryptoList = MutableLiveData<List<CryptoResponseItem>>()
+    val cryptoList: MutableLiveData<List<CryptoResponseItem>> get() = _cryptoList
+
+
+    private val _selectedCoin = MutableLiveData<CryptoResponseItem>()
+    val selectedCoin: MutableLiveData<CryptoResponseItem> get() = _selectedCoin
+
+    private val _marketData = MutableLiveData<MarketResponse>()
+    val marketData: MutableLiveData<MarketResponse> get() = _marketData
+
+    private val _error = MutableLiveData<String>()
+    val error: MutableLiveData<String> get() = _error
+
+    private fun handleCryptoListResponse(response: Response<CryptoResponse>) {
+        if (response.isSuccessful) {
+            response.body()?.let { cryptoResponse ->
+                _cryptoList.postValue(cryptoResponse)
+            }
+        } else {
+            _error.postValue(response.message())
+        }
+    }
+
+    private fun handleMarketResponse(response: Response<MarketResponse>) {
+        if (response.isSuccessful) {
+            response.body()?.let { marketResponse ->
+                _marketData.postValue(marketResponse)
+            }
+        } else {
+            _error.postValue(response.message())
+        }
+    }
+
+    fun getCryptoList(currency: String, order: String, numberPP: Int) {
+        viewModelScope.launch {
+            try {
+                val response = repo.getCryptoList(currency, order, numberPP)
+                handleCryptoListResponse(response)
+
+            } catch (e: Exception) {
+                _error.postValue(e.message ?: "Unknown error")
+            }
+        }
+    }
+
+    fun getMarketData() {
+        viewModelScope.launch {
+            try {
+                val response = repo.getMarketData()
+                handleMarketResponse(response)
+            } catch (e: Exception) {
+                _error.postValue(e.message ?: "Unknown error")
+            }
+        }
+    }
+
+    fun selectCoin(coin: CryptoResponseItem) {
+        _selectedCoin.value = coin
+    }
+}
