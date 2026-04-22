d0d78c269b4aa85d7e4516fd0ba9bbb65503bdca
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-04-22 14:36:41 +0500

    Added xml for main activity( set up Nav host Fragment), in main activity setup binding and VM

--- app/src/main/java/com/example/cryptohawk/MainActivity.kt
+++ app/src/main/java/com/example/cryptohawk/MainActivity.kt
@@ -5,16 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptohawk.Database.CryptoDatabase
+import com.example.cryptohawk.Repository.cryptoFactory
+import com.example.cryptohawk.Repository.cryptoRepository
+import com.example.cryptohawk.ViewModel.cryptoViewModel
+import com.example.cryptohawk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: cryptoViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setUpVM()
+    }
+    fun setUpVM(){
+        val repo= cryptoRepository(CryptoDatabase(this))
+        val factory= cryptoFactory(application, repo)
+        viewModel= ViewModelProvider(this, factory)[cryptoViewModel::class.java]
     }
 }
